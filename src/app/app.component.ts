c4ade9bc879b8d5467ef653512861fbaff74723a
Carlos Eduardo <carlospereira@pactosolucoes.com.br>
2022-09-25 14:50:39 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  userName : string = 'Joaquim';
+
   title = 'curso-angular';
 }
